bb6005f7c0e054317d8e7578f14cc20c4d2dab0d
Noah Cristino <noahcristino@yahoo.com>
2016-12-11 17:57:03 -0500

    Add files via upload

---
+++ sketch_dec11a.ino
@@ -0,0 +1,21 @@
+String data;   // for incoming serial data
+
+void setup() {
+        Serial.begin(9600);     // opens serial port, sets data rate to 9600 bps
+        pinMode(13, OUTPUT);
+}
+
+void loop() {
+
+        // send data only when you receive data:
+        if (Serial.available() > 0) {
+                  // read the incoming byte:
+                  data = Serial.readString();
+                  if (data == "on") {
+                        digitalWrite(13, HIGH);
+                  }
+                  if (data == "off") {
+                        digitalWrite(13, LOW);  
+                  }
+        }
+}
